8896b38171cbe13c03428b16a18f3e34f88069ad
thansen <thansen@solire.fr>
2015-09-18 15:18:09 +0200

    Adding jenkins build tools

---
+++ build-tools/jenkins/atoum.ci.php
@@ -0,0 +1,41 @@
+<?php
+// build-tools/jenkins/atoum.ci.php
+
+require_once __DIR__ . '/../../vendor/atoum/atoum/classes/autoloader.php';
+
+/*
+ * CLI report.
+ */
+$stdOutWriter = new \mageekguy\atoum\writers\std\out();
+$cli = new \mageekguy\atoum\reports\realtime\cli();
+$cli->addWriter($stdOutWriter);
+
+$basedir = __DIR__ . '/../../';
+
+/*
+ * Xunit report
+ */
+$xunit = new \mageekguy\atoum\reports\asynchronous\xunit();
+/*
+ * Xunit writer
+ */
+$writer = new \mageekguy\atoum\writers\file($basedir . '/build/logs/junit.xml');
+$xunit->addWriter($writer);
+
+/*
+ * Clover coverage
+ */
+$cloverWriter = new \mageekguy\atoum\writers\file($basedir . '/build/logs/clover.xml');
+$cloverReport = new atoum\reports\asynchronous\clover();
+$cloverReport->addWriter($cloverWriter);
+
+
+/*
+ * Html coverage
+ */
+$html = new \mageekguy\atoum\report\fields\runner\coverage\html('GocrPHP', $basedir . '/build/coverage');
+$cli->addField($html);
+
+$runner->addReport($xunit);
+$runner->addReport($cli);
+$runner->addReport($cloverReport);
