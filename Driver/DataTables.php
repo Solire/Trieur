958b49c67cfd53ef281e68924805da4a888990ae
Monnot Stéphane <monnot.stephane@gmail.com>
2015-10-27 18:52:11 +0100

    Datatable fix hidden column with responsive plugin

--- Driver/DataTables.php
+++ Driver/DataTables.php
@@ -175,7 +175,9 @@ class DataTables extends Driver
             ];
 
             if (isset($column->driverHidden) && $column->driverHidden) {
-                $dCol['visible'] = false;
+                $dCol['visible']   = false;
+                $dCol['className'] = 'never';
+
             }
 
             if (isset($column->width)) {
