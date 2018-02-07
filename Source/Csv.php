6673f98f366d7a908122aa58288a6724c809696b
Polinome <contact@polinome.com>
2018-02-07 16:28:45 +0100

    Parse csv file with header

--- Source/Csv.php
+++ Source/Csv.php
@@ -9,7 +9,7 @@ use Solire\Trieur\Exception;
 use Solire\Conf\Conf;
 
 /**
- * Csv connection wrapper
+ * Csv connection wrapper.
  *
  * @author  thansen <thansen@solire.fr>
  * @license MIT http://mit-license.org/
@@ -17,45 +17,52 @@ use Solire\Conf\Conf;
 class Csv extends Source
 {
     /**
-     * The csv file handle
+     * The csv file handle.
      *
      * @var resource
      */
     protected $handle = null;
 
     /**
-     * Number lines in the csv file
+     * Number lines in the csv file.
      *
      * @var int
      */
     protected $count = 0;
 
     /**
-     * Number lines matching the request in the csv file
+     * Number lines matching the request in the csv file.
      *
      * @var int
      */
     protected $filteredCount = 0;
 
     /**
-     * Lines of the csv file matching the request
+     * Header of the csv file.
+     *
+     * @var array
+     */
+    protected $header = [];
+
+    /**
+     * Lines of the csv file matching the request.
      *
      * @var array
      */
     protected $data = [];
 
     /**
-     * State of the request (to ensure we don't parse the file multiple times)
+     * State of the request (to ensure we don't parse the file multiple times).
      *
      * @var string
      */
     protected $md5 = null;
 
     /**
-     * Constructeur
+     * Constructeur.
      *
      * @param Conf    $conf       Configuration for the csv parse (length,
-     * delimiter, enclosure)
+     *                            delimiter, enclosure)
      * @param Columns $columns    Configuration des colonnes
      * @param string  $connection Chemin du fichier csv
      *
@@ -79,41 +86,85 @@ class Csv extends Source
         if (!isset($this->conf->length)) {
             $this->conf->length = 0;
         }
+
         if (!isset($this->conf->delimiter)) {
             $this->conf->delimiter = ',';
         }
+
         if (!isset($this->conf->enclosure)) {
             $this->conf->enclosure = '"';
         }
+
+        if (!isset($this->conf->headerRow)) {
+            $this->conf->headerRow = null;
+        } elseif (!isset($this->conf->headerLength)) {
+            $this->conf->headerLength = (int) $this->conf->headerRow + 1;
+        }
+
+        if (!isset($this->conf->headerLength)) {
+            $this->conf->headerLength = 0;
+        }
+
+        $this->handle();
     }
 
     /**
-     * Opens the csv file
+     * Opens the csv file.
      *
      * @return void
      */
     protected function handle()
     {
         $this->handle = fopen($this->connection, 'r');
+
+        for ($ii = 0; $ii < $this->conf->headerLength; $ii++) {
+            $row = $this->fetch(true);
+            if ($this->conf->headerRow === $ii) {
+                $this->header = $row;
+            }
+        }
     }
 
     /**
-     * Fetches a line from csv file
+     * Fetches a line from csv file.
      *
-     * @return array
+     * @param bool $raw Do not change keys with header cells
+     *
+     * @return bool
+     *
+     * @throws Exception
      */
-    protected function fetch()
+    protected function fetch($raw = false)
     {
-        return fgetcsv(
+        $row = fgetcsv(
             $this->handle,
             $this->conf->length,
             $this->conf->delimiter,
             $this->conf->enclosure
         );
+
+        if ($raw || empty($this->header)) {
+            return $row;
+        }
+
+        if ($row === false) {
+            return false;
+        }
+
+        $formatedRow = [];
+        foreach ($row as $ii => $cell) {
+            if (!isset($this->header[$ii])) {
+                throw new Exception(sprintf('no offset [%s] in %s', $ii, print_r($this->header, true)));
+            }
+
+            $formatedRow[$this->header[$ii]] = $cell;
+        }
+
+        return $formatedRow;
     }
 
     /**
-     * Closes the handle to the csv file
+     * Closes the handle to the csv file.
      *
      * @return void
      */
@@ -123,7 +174,7 @@ class Csv extends Source
     }
 
     /**
-     * Return the total of available lines
+     * Return the total of available lines.
      *
      * @return int Total number
      */
@@ -135,7 +186,7 @@ class Csv extends Source
     }
 
     /**
-     * Return the total of available lines filtered by the current filters
+     * Return the total of available lines filtered by the current filters.
      *
      * @return int Total number
      */
@@ -147,18 +198,19 @@ class Csv extends Source
     }
 
     /**
-     * Returns the data filtered by the current filters
+     * Returns the data filtered by the current filters.
      *
      * @return mixed
      */
     public function getData()
     {
         $this->parse();
+
         return $this->data;
     }
 
     /**
-     * Add a row to the data following the defined orders
+     * Add a row to the data following the defined orders.
      *
      * @param array $newRow The row
      *
@@ -167,6 +219,7 @@ class Csv extends Source
     protected function addToEligible($newRow)
     {
         $newOffset = count($this->data);
+
         foreach ($this->data as $offset => $row) {
             if ($this->lowerThan($newRow, $row)) {
                 $newOffset = $offset;
@@ -178,7 +231,7 @@ class Csv extends Source
     }
 
     /**
-     * Inserts a row in the data at a defined offset
+     * Inserts a row in the data at a defined offset.
      *
      * @param array $row    The row
      * @param int   $offset The offset
@@ -195,14 +248,14 @@ class Csv extends Source
     }
 
     /**
-     * Compare two rows of data
+     * Compare two rows of data.
      *
      * @param type $row1 The first row
      * @param type $row2 The second row
      *
      * @return bool Returns true if $row1 is less than $row2,
-     * false if $row1 is greater or equal than $row2, following the defined
-     * orders
+     *              false if $row1 is greater or equal than $row2, following the defined
+     *              orders
      */
     protected function lowerThan($row1, $row2)
     {
@@ -214,8 +267,8 @@ class Csv extends Source
             list($column, $dir) = $order;
 
             $test = strnatcasecmp(
-                $row1[$column->sourceName],
-                $row2[$column->sourceName]
+                $this->getCell($row1, $column->sourceName),
+                $this->getCell($row2, $column->sourceName)
             );
 
             if ($test == 0) {
@@ -233,7 +286,7 @@ class Csv extends Source
     }
 
     /**
-     * Parses the csv file, and build the data array
+     * Parse the csv file, and build the data array.
      *
      * @return void
      */
@@ -256,6 +309,7 @@ class Csv extends Source
         $this->handle();
 
         $this->data = [];
+
         while ($this->row = $this->fetch()) {
             if ($this->filter()) {
                 $this->addToEligible($this->row);
@@ -274,7 +328,7 @@ class Csv extends Source
     }
 
     /**
-     * Process the filter
+     * Process the filter.
      *
      * @param Filter $filter The filter class
      *
@@ -283,6 +337,31 @@ class Csv extends Source
     protected function processFilter(SourceFilter $filter)
     {
         $filter->setRow($this->row);
+
         return $filter->filter();
+    }
+
+    /**
+     * Get cell in a row by its index or key (column header cell value).
+     *
+     * @param array      $row        Row
+     * @param string|int $indexOrKey Index or key
+     *
+     * @return type
+     *
+     * @throws Exception
+     */
+    protected function getCell($row, $indexOrKey)
+    {
+        $index = $indexOrKey;
+        if (!empty($this->header)) {
+            $index = array_search($indexOrKey, $this->header);
+
+            if ($index === false) {
+                throw new Exception(sprintf('header key [%s] does not exist', $indexOrKey));
+            }
+        }
+
+        return $row[$indexOrKey];
     }
 }
